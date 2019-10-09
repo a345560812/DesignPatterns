b43e545d7d6c4f029bd568fa1ad00e365924d3d2
unknown <zhoutao_sx@qiyi.com>
2019-10-09 20:22:38 +0800

    strategy pattern add details description

--- src/main/java/com/xiaoming/pattern/strategy/Main.java
+++ src/main/java/com/xiaoming/pattern/strategy/Main.java
@@ -4,6 +4,7 @@ package com.xiaoming.pattern.strategy;
 ②策略模式的最大特点是使得算法可以在不影响客户端的情况下发生变化，从而改变不同的功能。
 ③体现了封装变化的概念以及编程中使用接口，而不是使用的是具体的实现类(面向接口编程)。
 ④可以回忆一下TreeSet的设计，我们可以自己实现Comparator接口来控制该集合接收元素的语义。
+⑤ 在CashContext类的构造函数当中又使用了简单工厂模式， 完成了简单工厂模式和策略模式的结合使用。
 */
 public class Main {
     public static void main(String[] args) {
